7c6e17c3c74c551c5cff35417a4f5c06d56cfefc
noahhi <noahhuntisaak@gmail.com>
2019-10-14 19:34:26 -0400

    re-named variables to be more consistent with the literature

--- cubics.py
+++ cubics.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.random import randint
 
+
 class CMDKP:
   '''
   Used to represent an instance of the Cubic Multidimensional Knapsack Problem
@@ -18,55 +19,31 @@ class CMDKP:
     self.density = density
     self.constraints = constraints
 
-    self.P1 = np.zeros(n)
-    self.P2 = np.zeros((n,n))
-    self.P3 = np.zeros((n,n,n))
+    self.c = np.zeros(n)
+    self.C = np.zeros((n,n))
+    self.D = np.zeros((n,n,n))
 
     for i in range(n):
-      if np.random.randint(1, 101) <= density:
-        self.P1[i] = np.random.randint(low=1,high=101)
+      if randint(1, 101) <= density:
+        self.c[i] = randint(low=1,high=101)
       for j in range(i+1, n):
-        if np.random.randint(1, 101) <= density:
-          self.P2[i,j] = np.random.randint(low=1,high=101)
+        if randint(1, 101) <= density:
+          self.C[i,j] = randint(low=1,high=101)
         for k in range(j+1, n):
-          if np.random.randint(1, 101) <= density:
-            self.P3[i,j,k] = np.random.randint(low=1,high=101)
-
-    # # generate linear, quadratic, and cubic profit matrices
-    # self.P1 = np.random.randint(low=1, high=101, size=n)
-    # self.P2 = np.random.randint(low=1, high=101, size=(n,n))
-    # np.fill_diagonal(self.P2,0)
-    # self.P3 = np.random.randint(low=1, high=101, size=(n,n,n))
-    # np.fill_diagonal(self.P3,0)
-    #
-    # # helper function to replace values with 0s according to density
-    # def add_zeros(x):
-    #   if np.random.randint(low=1,high=101) > density:
-    #     return 0
-    #   else:
-    #     return x
-    # vectorized_zero = np.vectorize(add_zeros)
-    #
-    # self.P1 = vectorized_zero(self.P1)
-    # self.P2 = vectorized_zero(self.P2)
-    # self.P3 = vectorized_zero(self.P3)
+          if randint(1, 101) <= density:
+            self.D[i,j,k] = randint(low=1,high=101)
 
     # generate item weights (each item has a weight in each dimension)
-    self.w = np.random.randint(low=1, high=51, size=(n,constraints))
+    self.a = np.random.randint(low=1, high=51, size=(n,constraints))
 
     # generate knapsack capacities (one for each dimension)
-    self.C = np.zeros(constraints)
+    self.b = np.zeros(constraints)
     for j in range(constraints):
-      self.C[j] = np.random.randint(low=50, high=sum(self.w[i,j] for i in range(n))) #TODO what should lower bound be? 50 -> errors for small n
+      self.b[j] = np.random.randint(low=50, high=sum(self.a[i,j] for i in range(n))) #TODO what should lower bound be? 50 -> errors for small n
 
 
 def main():
     cdmkp = CMDKP(n=4,density=80,constraints=2,seed=1)
-    print(cdmkp.P1)
-    print()
-    print(cdmkp.P2)
-    print()
-    print(cdmkp.P3)
 
 
 if __name__=="__main__":
--- gurobi_solve.py
+++ gurobi_solve.py
@@ -8,11 +8,11 @@ def adams_and_forrester_lin(cubic):
     # retrieve info about the instance
     n = cubic.n
     constraints = cubic.constraints
-    P1 = cubic.P1
-    P2 = cubic.P2
-    P3 = cubic.P3
-    w = cubic.w
+    c = cubic.c
     C = cubic.C
+    D = cubic.D
+    a = cubic.a
+    b = cubic.b
 
     # create a new empty model
     m = Model()
@@ -22,16 +22,16 @@ def adams_and_forrester_lin(cubic):
     U_hat = np.zeros((n,n))
     for i in range(n):
         for j in range(n):
-            P3_row = P3[i][j]
-            pos_indices = P3_row > 0
-            U_hat[i][j] = sum(P3_row[pos_indices])
-            neg_indices = P3_row < 0
-            L_hat[i][j] = sum(P3_row[neg_indices])
+            D_row = D[i][j]
+            pos_indices = D_row > 0
+            U_hat[i][j] = sum(D_row[pos_indices])
+            neg_indices = D_row < 0
+            L_hat[i][j] = sum(D_row[neg_indices])
 
     # compute L_bar and U_bar
     L_bar = np.zeros(n)
     U_bar = np.zeros(n)
-    combined = P2 + U_hat
+    combined = C + U_hat
     for j in range(n):
         for i in range(n):
             U_bar[j] += max(combined[i][j], 0)
@@ -42,18 +42,30 @@ def adams_and_forrester_lin(cubic):
     psi = m.addVars(n, vtype=GRB.CONTINUOUS)
     tau = m.addVars(n, n, vtype=GRB.CONTINUOUS)
 
+    for d in range(constraints):
+        m.addLConstr(quicksum(x[i]*a[i][d] for i in range(n)) <= b[d])
+
+    g = [None] * n
+    for j in range(n):
+        g[j] = quicksum(C[i][j]*x[i] for i in range(n))
+
+    h = [[None] * n for i in range(n)]
+    for j in range(n):
+        for i in range(n):
+            h[i][j] = quicksum(D[i][j][k]*x[k] for k in range(n))
+
     # add constraints
     for j in range(n):
-        g_j = LinExpr([P2[i][j] for i in range(n)],x.select())
-        m.addConstr(psi[j] >= L_bar[j]+x[j]*(U_bar[j]-L_bar[j])-g_j-sum(U_hat[i][j]*x[i]-tau[i,j] for i in range(n)))
+        m.addConstr(psi[j] >= L_bar[j]+(x[j]*(U_bar[j]-L_bar[j]))-g[j]-sum(U_hat[i][j]*x[i]-tau[i,j] for i in range(n) if i != j))
         m.addConstr(psi[j] >= 0)
         for i in range(n):
-            h_ij = LinExpr([P3[i][j][k] for k in range(n)],x.select())
-            m.addConstr(tau[i,j] >= L_hat[i][j]+x[i]*(U_hat[i][j]-L_hat[i][j])-h_ij)
+            if i == j:
+                continue
+            m.addConstr(tau[i,j] >= L_hat[i][j]+(x[i]*(U_hat[i][j]-L_hat[i][j]))-h[i][j])
             m.addConstr(tau[i,j] >= 0)
 
     # set objective
-    m.setObjective(quicksum(x[j]*(P1[j]+U_bar[j])-psi[j] for j in range(n)), sense=GRB.MAXIMIZE)
+    m.setObjective(quicksum(x[j]*(c[j]+U_bar[j])-psi[j] for j in range(n)), sense=GRB.MAXIMIZE)
 
     return m
 
@@ -64,11 +76,11 @@ Apply the standard linearization to an instance of the cubic Multidimensional kn
 def standard_lin(cubic):
     n = cubic.n
     constraints = cubic.constraints
-    P1 = cubic.P1
-    P2 = cubic.P2
-    P3 = cubic.P3
-    w = cubic.w
+    c = cubic.c
     C = cubic.C
+    D = cubic.D
+    a = cubic.a
+    b = cubic.b
 
     model = Model()
 
@@ -77,29 +89,24 @@ def standard_lin(cubic):
     z = model.addVars(n,n,n, name="cubic_var", vtype=GRB.CONTINUOUS)
 
     for d in range(constraints):
-        model.addLConstr(lhs=LinExpr([w[i][d] for i in range(n)],x.select()), rhs=C[d], sense=GRB.LESS_EQUAL)
-        #model.addLConstr(quicksum(x[i]*w[i][d] for i in range(n)) <= C[d])
+        model.addLConstr(quicksum(x[i]*a[i][d] for i in range(n)) <= b[d])
 
-    # TODO skip when i=j?
     for i in range(n):
-        for j in range(n):
+        for j in range(i+1,n):
             model.addLConstr(y[i,j] <= x[i])
             model.addLConstr(y[i,j] <= x[j])
             model.addLConstr(y[i,j] >= x[i] + x[j] - 1)
             model.addLConstr(y[i,j] >= 0)
-            for k in range(n):
+            for k in range(j+1,n):
                 model.addLConstr(z[i,j,k] <= x[i])
                 model.addLConstr(z[i,j,k] <= x[j])
                 model.addLConstr(z[i,j,k] <= x[k])
                 model.addLConstr(z[i,j,k] >= x[i] + x[j] + x[k] - 2)
                 model.addLConstr(z[i,j,k] >= 0)
 
-    linear_values = quicksum(P1[i]*x[i] for i in range(n))
-    quad_values = quicksum(P2[i][j]*y[i,j] for i in range(n-1) for j in range(i+1,n))
-    cubic_values = quicksum(P3[i][j][k]*z[i,j,k] for i in range(n-2) for j in range(i+1,n-1) for k in range(j+1,n))
-    #linear_values = LinExpr([P1[i] for i in range(n)],x.select())
-    #quad_values = LinExpr([P2[i][j] for i in range(n) for j in range(n)],y.select())
-    #cubic_values = LinExpr([P3[i][j][k] for i in range(n) for j in range(n) for k in range(n)],z.select())
+    linear_values = quicksum(c[i]*x[i] for i in range(n))
+    quad_values = quicksum(C[i][j]*y[i,j] for i in range(n-1) for j in range(i+1,n))
+    cubic_values = quicksum(D[i][j][k]*z[i,j,k] for i in range(n-2) for j in range(i+1,n-1) for k in range(j+1,n))
     model.setObjective(linear_values+quad_values+cubic_values, sense=GRB.MAXIMIZE)
 
     return model
@@ -109,20 +116,9 @@ def main():
     setParam('OutputFlag',0)
     setParam('LogFile',"")
 
-    # generate instance od CMDKP
-    n = 4
+    # generate instance of CMDKP
+    n = 15
     cdmkp = CMDKP(n=n,density=70,constraints=1)
-
-    print()
-    print(cdmkp.w)
-    print()
-    print(cdmkp.C)
-    print()
-    print(cdmkp.P1)
-    print()
-    print(cdmkp.P2)
-    print()
-    print(cdmkp.P3)
 
     # model and solve using the Adams+Forrester linearization
     model1 = standard_lin(cdmkp)
